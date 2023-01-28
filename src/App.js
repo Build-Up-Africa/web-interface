9e318009367e61b0f476f9fc52de3e1bd7ec5923
mrolaleyeisrael <mrolaleyeisrael@gmail.com>
2023-01-28 16:18:57 +0100

    Re-installed react, installed Tailwind instaede of Material UI

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+	return (
+		<div className='App'>
+			<h2 className=' text-center text-2xl'>Build-Up Africa</h2>
+		</div>
+	);
+}
+
+export default App;
